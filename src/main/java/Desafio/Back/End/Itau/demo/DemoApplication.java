3f3345114fb21b274e557c4d3ef886bde23f2f47
kaio-ksianskievis <ksianskieviskaio@gmail.com>
2025-06-20 22:48:51 -0300

    CREATE docs/arquivos iniciais

---
+++ src/main/java/Desafio/Back/End/Itau/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package Desafio.Back.End.Itau.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
